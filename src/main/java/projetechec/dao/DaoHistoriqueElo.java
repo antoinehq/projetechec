2532b9969b990c19f944bb0f5c5c48b6b2f5f9bf
antoinehq <antoine.herlicq@gmail.com>
2020-04-28 18:34:57 +0200

    Dao Historique

---
+++ src/main/java/projetechec/dao/DaoHistoriqueElo.java
@@ -0,0 +1,8 @@
+package projetechec.dao;
+
+import projetechec.entity.HistoriqueElo;
+import projetechec.entity.HistoriqueEloKey;
+
+public interface DaoHistoriqueElo extends DaoGeneric<HistoriqueElo, HistoriqueEloKey>{
+
+}
