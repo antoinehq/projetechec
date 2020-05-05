fd562b5ba11b6c3298fded5cca56a271e2bf19ba
svenos7 <sven_charmeteau@msn.com>
2020-05-05 09:57:52 +0200

    repo partie

---
+++ src/main/java/projetechec/repository/PartieRepository.java
@@ -0,0 +1,9 @@
+package projetechec.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import projetechec.entity.Partie;
+
+public interface PartieRepository extends JpaRepository<Partie, Integer> {
+
+}
