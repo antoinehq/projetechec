492ba682f1f3d8a1eeb119381b4b00a9e47815bf
antoinehq <antoine.herlicq@gmail.com>
2020-05-05 17:28:04 +0200

    update du jour

--- src/main/java/projetechec/repository/MembreRepository.java
+++ src/main/java/projetechec/repository/MembreRepository.java
@@ -1,9 +1,39 @@
 package projetechec.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import projetechec.entity.Club;
 import projetechec.entity.Membre;
+import projetechec.entity.Tournoi;
 
 public interface MembreRepository extends JpaRepository<Membre, Integer>{
 
+	Optional<Membre> findByNomAndPrenom(String nom, String prenom);
+	
+	List<Membre> findByClub(Club club);
+	
+	List<Membre> findByElo(Integer elo);
+	
+	@Query("select elo from HistoriqueElo e where e.membre= :id")
+	Integer findEloMaxById(@Param("id")Integer id);
+	
+	@Query()
+	List<Membre> findByTournoi(Tournoi tournoi);
+	
+	Integer countParties(Integer id);
+	
+	Integer countVictoires(Integer id);
+	
+	Integer countNuls(Integer id);
+	
+	Integer countDefaites(Integer id);
+	
+	Double pourcentageVictoire(Integer id);
+
+	Integer countTournoi(Integer id);
 }
--- src/main/java/projetechec/service/MembreService.java
+++ src/main/java/projetechec/service/MembreService.java
@@ -1,5 +1,56 @@
 package projetechec.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Service;
+
+import projetechec.entity.Club;
+import projetechec.entity.Membre;
+import projetechec.repository.MembreRepository;
+
+@Service
 public class MembreService {
 
+	@Autowired
+	private MembreRepository membreRepository;
+	
+	public Membre findById(Integer id) {
+		Optional<Membre> opt= membreRepository.findById(id);
+		if (opt.isPresent()) {
+			return opt.get();
+		}
+		throw new IllegalArgumentException();
+	}
+	
+	public Membre findByNomAndPrenom(String nom, String prenom) {
+		Optional<Membre> opt= membreRepository.findByNomAndPrenom(nom, prenom);
+		if (opt.isPresent() ) {
+			return opt.get();
+		}
+		throw new IllegalArgumentException();
+	}
+	
+	public List<Membre> findByClub(Club club) {
+		List<Membre> membres = membreRepository.findByClub(club);
+		if (membres.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		return membres;
+	}
+	
+	public List<Membre> findByElo(Integer elo) {
+		List<Membre> membres = membreRepository.findByElo(elo);
+		if (membres.isEmpty()) {
+			throw new IllegalArgumentException();
+		}
+		return membres;
+	}
+	
+	public Integer findEloMaxById(Integer id) {
+		List<Membre> membres = membreRepository.findEloMaxById(id);
+	}
+	
 }
