fddd51fb972429fa7e4c916c19f9f55de0302975
svenos7 <sven_charmeteau@msn.com>
2020-05-05 16:47:37 +0200

    repo club et tournoi

--- src/main/java/projetechec/repository/ClubRepository.java
+++ src/main/java/projetechec/repository/ClubRepository.java
@@ -1,9 +1,32 @@
 package projetechec.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import projetechec.entity.Club;
 
-public interface ClubRepository extends JpaRepository<Club, Integer>{
+public interface ClubRepository extends JpaRepository<Club, Integer> {
+	@Query("select c from club c left join fetch c.pays_club where c.pays_club=:pays")
+	List<Club> findByPays(@Param("pays") String pays, Pageable pageable);
+
+	@Query("select c from club c left join fetch c.ville_club where c.ville_club=:ville")
+	List<Club> findByVille(@Param("ville") String ville);
+
+	@Query("select c from club c left join fetch c.nom_club where c.nom_club=:nom")
+	Optional<Club> findByNom(@Param("nom") String nom);
+
+	@Query("select c count(*) from club")
+	Integer countById();
+
+	@Query("select c count(membres) from club")
+	Integer countAllMembreByClub();
+
+	@Query("select c count(tournois) from club")
+	Integer countAllTournoiByClub();
 
 }
--- src/main/java/projetechec/repository/TournoiRepository.java
+++ src/main/java/projetechec/repository/TournoiRepository.java
@@ -1,9 +1,32 @@
 package projetechec.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import projetechec.entity.Arbitre;
+import projetechec.entity.Cadence;
+import projetechec.entity.Club;
+import projetechec.entity.Participation;
 import projetechec.entity.Tournoi;
 
-public interface TournoiRepository extends JpaRepository<Tournoi, Integer>{
+public interface TournoiRepository extends JpaRepository<Tournoi, Integer> {
+	@Query("select t from tournoi t left join fetch t.date_tournoi where t.date_tournoi=:date")
+	List<Tournoi> findByDate(@Param("date") Date date);
+
+	@Query("select t from tournoi t left join fetch t.cadence_tournoi where t.cadence_tournoi=:cadence")
+	List<Tournoi> findByCadence(@Param("cadence") Cadence cadence);
+
+	@Query("select t from tournoi t left join fetch t.organisateur where t.organisateur=:club")
+	List<Tournoi> findByClub(@Param("club") Club club);
+
+	@Query("select t from tournoi t left join fetch t.arbitre_tournoi where t.arbitre_tournoi=:arbitre")
+	List<Tournoi> findByArbitre(@Param("arbitre") Arbitre arbitre);
+
+	@Query("select t from tournoi c left join fetch membre where t.participations=:participations")
+	List<Tournoi> findByMembre(@Param("participations") Participation participations);
 
 }
