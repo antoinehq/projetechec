282c0b8aa6e865156864615a49efb88ded264d93
LoicRenault <renault.loic.acm@gmail.com>
2020-05-05 09:35:52 +0200

    les configs

---
+++ src/main/java/projetechec/config/AppConfig.java
@@ -0,0 +1,77 @@
+package projetechec.config;
+
+import java.util.Properties;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManagerFactory;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@PropertySource("classpath:config.properties")
+@EnableJpaRepositories(basePackages = { "projetechec.repository" })
+public class AppConfig {
+	@Autowired
+	private Environment env;
+
+	@PostConstruct
+	public void init() {
+//		System.out.println(url);
+	}
+
+	@Bean
+	public BasicDataSource dataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(env.getProperty("dataSource.driver"));
+		dataSource.setUrl(env.getProperty("dataSource.url"));
+		dataSource.setUsername(env.getProperty("dataSource.username"));
+		dataSource.setPassword(env.getProperty("dataSource.password"));
+		dataSource.setMaxTotal(10);
+		dataSource.setInitialSize(5);
+		return dataSource;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(BasicDataSource dataSource) {
+		LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+		emf.setDataSource(dataSource);
+		emf.setPackagesToScan("projetechec.entity");
+		emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		emf.setJpaProperties(null);
+		return emf;
+	}
+
+	private Properties jpaProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "validate");
+		properties.setProperty("hibernate.dialect", env.getProperty("dialect"));
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.format_sql", "true");
+		return properties;
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+		return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+}
